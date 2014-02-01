03586b215318c3d7e2f2a728240a93db7079bed8
ryo <kurumapureo@yahoo.co.jp>
2014-02-02 00:57:48 +0900

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ DemoApp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+config.logger = Logger.new(STDOUT)
+config.logger.level = Logger.const_get(ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'INFO')
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
