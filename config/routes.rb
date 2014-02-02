ea17da8e14f4ce0cfbf56dc4b3d2f5d7932eccb8
ryo <kurumapureo@yahoo.co.jp>
2014-02-02 10:45:47 +0900

    OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 DemoApp::Application.routes.draw do
   resources :users
-
-
+  #resources :users, :only => [ :index ]
+  root :to => "users#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
